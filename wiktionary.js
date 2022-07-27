ee6f1faf3fc5c41db558f044e5f826ef3febb907
christopherfelt <chris.h.felt@gmail.com>
2022-07-27 06:58:25 -0600

    figuring out algorithm

--- wiktionary.js
+++ wiktionary.js
@@ -1,11 +1,18 @@
 import axios from "axios"
 import {load} from "cheerio";
 
-axios.get('https://en.wiktionary.org/w/api.php?action=parse&format=json&uselang=user&page=apple').then(resp => {
+axios.get('https://en.wiktionary.org/w/api.php?action=parse&format=json&uselang=user&page=cintura').then(resp => {
 
     let textData = resp.data.parse.text['*']
 
     let ch = load(textData);
 
-    console.log(ch("#Etymology").parent().next().html());
+    console.log(ch("#Etymology").parent().next().next().next().html());
 });
+
+// get id
+// go up a parent
+// go to next until p tag
+// go to child and check if span and span class equals etyl
+// go to child for anchor tag
+// get text
