63a466add451b3555768a1f3d24800982824d9b7
christopherfelt <chris.h.felt@gmail.com>
2022-08-02 07:04:11 -0600

    complete search section

--- wiktionary.js
+++ wiktionary.js
@@ -6,60 +6,53 @@ axios.get('https://en.wiktionary.org/w/api.php?action=parse&format=json&uselang=
     let textData = resp.data.parse.text['*']
     
     let ch = load(textData);
+    
+    
+    let searchLanguage = "Spanish";
+    let languageHeader;
 
-    // console.log(ch("#Etymology").parent().next().next().next().html());
-    // console.log(ch("[id^='Etymology']").forEach(
-    //     console.log
-    // ));
     var etList = ch("[id^='Etymology']");
-
-
+    
+    // For each Etymology element, the nearest H2 header and check the language name.  If language name matches search language proceed. Stop if the HR or toc element are found first
     etList.each(function(){
         let element = ch(this);
         let startingPosition = element.parent().prev();
-
+        
         let counter = 0;
         let proceed = true;
-
-        // I am not quite sure why I went this way
-        // But a better route will be to look for the language header and go through a list of eytomology
+        let language;
+        
 
         while(proceed && counter < 10){
             
             let tagName = startingPosition.prop('tagName')
             let isTOC = startingPosition.attr('id') == 'toc'
-
-            console.log(tagName)
             
             if(tagName == "H2"){
                 let firstChild = ch(startingPosition.children()[0])
                 console.log(firstChild.html());
                 proceed = false;
+                language = firstChild.html().trim();
+                languageHeader = element;
             } else if(tagName == "HR"){
                 proceed == false;
             } else if (isTOC){
                 proceed == false;
-            } else (
-                startingPosition.prev()
-            )
+            } else {
+                startingPosition = startingPosition.prev()
+            }
 
             counter++;
-
         }
 
-
-
-
-        // .children().each(function(){
-        //     let languageHeader = ch(this)
-        //     console.log(languageHeader.html())
-        // })
+        if(language == searchLanguage) return false;
     })
+
+    console.log(languageHeader.parent().next().html());
+
+    // loop through children and get the text
+
+
+
 });
 
-// get id
-// go up a parent
-// go to next until p tag
-// go to child and check if span and span class equals etyl
-// go to child for anchor tag
-// get text
